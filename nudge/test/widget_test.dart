b7e555ab93db687a9717e90e7af2f3bec1a17ef9
sreyasa2 <sreyasa2@illinois.edu>
2020-08-16 01:37:04 +0530

    Added file

--- nudge/test/widget_test.dart
+++ nudge/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nudge/main.dart';
+import 'package:nudge/sample.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
