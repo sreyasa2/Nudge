77e294dbe98ebc826ed8943ee3b43a1f199fc47a
sreyasa2 <sreyasa2@illinois.edu>
2020-08-19 00:13:56 +0530

    Added SplashScreen

--- nudge/lib/main.dart
+++ nudge/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -8,34 +9,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Base-Container',
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        accentColor: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Column(
-          children: [
-              Center(child: imageSection,)
-          ],
-        )
-        /* appBar: AppBar(
-            title: Text('Nudge',
-                style: TextStyle(
-                    color: const Color(0xffFFD700),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40))), */
-      ),
+        debugShowCheckedModeBanner: false,
+        title: 'Base-Container',
+        home: SplashScreen());
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  Future<Timer> loadData() async {
+    return new Timer(Duration(seconds: 7), onDoneLoading);
+  }
+
+  onDoneLoading() async {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xffCD5914),
+      child: imageSection,
     );
   }
 
   final Widget imageSection = Container(
       child: Image.asset(
-    'images/nudge.jpg',
+    'images/nudge_final.jpeg',
     height: 250,
     width: 400,
   ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Homescreen',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
 }
